78d08e9afd131668d96d758ddadfc3e4177bbfa8
JJINE <51090557+JJine@users.noreply.github.com>
2026-04-24 05:48:50 +0900

    update 260424

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next'
 import './globals.css'
 import Header from '@/components/layout/Header'
+import LoadingScreen from '@/components/ui/LoadingScreen';
 import CursorAnimation from '@/components/ui/CursorAnimation'
 
 export const metadata: Metadata = {
@@ -18,6 +19,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         <link rel="preconnect" href="https://fonts.googleapis.com" />
       </head>
       <body>
+        <LoadingScreen/>
         <CursorAnimation />
         <Header />
         <main style={{ paddingTop: 'var(--header-h)' }}>
